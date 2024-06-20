2c43894979248790ac2ee422b80ae2bf78b1488c
Sam-MARTis <samanthmartis@gmail.com>
2024-06-20 23:37:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+class Point{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+}
+
+class QuadTree{
+    constructor(x, y, width, height){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+}
